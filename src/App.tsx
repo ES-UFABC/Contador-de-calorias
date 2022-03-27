bcc89ad9ecb2adcf1327717d26bc29954660fd70
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-03-27 16:12:45 -0300

    login page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import Rotas from './rotas';
 import Header from "./components/Header";
+import { AuthProvider } from './context/AuthProvider';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ProtectedLayout } from './components/Protected';
+import { Login } from './components/Login';
+import Sobre from './pages/Sobre';
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Rotas />      
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/profile' element={
+            <ProtectedLayout>
+              <Sobre/>
+            </ProtectedLayout>}/>
+          <Route path='/login' element={<Login/>}>
+          </Route>
+        </Routes>     
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
