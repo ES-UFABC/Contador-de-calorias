65cd7956d0e1bbf7c275e997ae00bdeceb6d1118
Lucas Martin De Lucca <66978822+LucasMDeLucca@users.noreply.github.com>
2022-04-05 21:16:54 -0300

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,11 +1,22 @@
 from web_scraping import get_calories
+import json
 
-info = get_calories(item = '1755-arroz-carreteiro')
+f = open("//wsl.localhost/Ubuntu-20.04/home/delucca/Workspace/ufabc/eng_sof/Contador-de-calorias/src/urls.json")
 
-print('Tabela Nutricional arroz carreteiro:')
+x = json.load(f)
+
+z = list(x.keys())[14]
+y = x[z]
+
+
+info = get_calories(item = str(y['url']))
+
+print('Tabela Nutricional '+ str(z) + ":")
 print('----------------------------------------------------')
+print('Medida: ' + str(y['unity']))
 
 print('Calorias: ' , info.cal_value ,'kcal')
 print('Carboidratos: ' , info.carbo_value ,'g')
 print('Proteinas: ' , info.prot_value ,'g')
 print('Gorduras: ' , info.lip_value ,'g')
+
