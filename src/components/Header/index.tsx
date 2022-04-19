70dd1ce29847baca172e053b43b4c32a7403b638
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-04-18 23:33:00 -0300

    added logout and welcome message

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,21 @@
 import "./styles.css";
 import logo from "../../assets/Logo-2.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Header(){
+    const userName = localStorage.getItem('userName');
+    const navigate = useNavigate();
+
+
+    async function logout(){
+        try{
+            localStorage.clear();
+            navigate('/');
+        }catch(err){
+            alert('Logout failed');
+        }
+    }
+
     return(
         <header className="container">
             <h1 className="nome-proj">Contador de Calorias</h1>
@@ -12,6 +26,7 @@ export default function Header(){
                         <img className="img" src={logo} alt="Logo"/>                        
                     </div>
 
+                    <span>Welcome, <strong>{userName !== null ? userName.toUpperCase():''}</strong>!</span>
                     <div className="topicos">
                         <p>Home</p>                  
                     </div>
@@ -24,10 +39,8 @@ export default function Header(){
                         <p>Historico</p>                  
                     </div>
 
-                    <div className="topicos">
-                        <p>Login</p>                  
-                    </div>
 
+                    <button onClick={logout}>Logout</button>
                 </div>
             </section>
 
