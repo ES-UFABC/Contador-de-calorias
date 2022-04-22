7a3e2bba2763de3b64b71c598cb3d123b4f7e0b1
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-04-22 19:13:16 -0300

    improved the select button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,16 @@
 import { useState, useEffect } from "react";
 import { Api } from "../../services/api";
+import { Select } from "antd";
+
+export interface IFood{
+    name: string,
+    cal_val: number,
+    carbo_val: number,
+    lip_val: number,
+}
 
 export default function Home(){
-    const [foods,setFoods] = useState([]);
+    const [foods,setFoods] = useState<IFood[]>([]);
 
     const token = localStorage.getItem('token');
 
@@ -12,22 +20,28 @@ export default function Home(){
         }
     }
 
+    function handleChange(){
+        console.log('item selected')
+    }
+
    useEffect(()=>{
     async function chargeFoods(){
         const response = await Api.get("getFoods",authorization);
         setFoods(response.data);
-        console.log(response);
-        console.log(foods); 
     }
     
     chargeFoods();
     },[]);
-
+    console.log(foods)
     return(
         <div>
             <h1>Home</h1>
-            <button>Foods List</button>
-
+            {/* foods.map((result)=>(<option>{result.data}</option>)) */}
+            <Select  onChange={handleChange} defaultValue="Escolha um Alimento">
+                {foods.map(food =>
+                <Select.Option key={food.name} value={food.name}>{food.name}</Select.Option>
+                )};
+            </Select>
         </div>
     );
 }
