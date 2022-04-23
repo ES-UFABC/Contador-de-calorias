89b5743e71c5305b356b31b03076c8f3781d367b
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-04-23 20:34:19 -0300

    added foods selected list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, SetStateAction } from "react";
 import { Api } from "../../services/api";
 import { Select } from "antd";
+import {FaTrash} from 'react-icons/fa';
+import { List, DeleteButton } from "./styles";
 
 export interface IFood{
     name: string,
@@ -10,7 +12,8 @@ export interface IFood{
 }
 
 export default function Home(){
-    const [foods,setFoods] = useState<IFood[]>([]);
+    const [allFoods,setAllFoods] = useState<IFood[]>([]);
+    const [myFoods, setMyFoods] = useState<IFood[]>([]);
 
     const token = localStorage.getItem('token');
 
@@ -20,28 +23,61 @@ export default function Home(){
         }
     }
 
-    function handleChange(){
-        console.log('item selected')
+    //function handleInputChange(e: SetStateAction<IFood[]>){
+    /*function handleInputChange(e:IFood){
+        console.log('1---')
+        console.log(e)
+        //console.log(e.target)
+        //console.log(e.target.value)
+        console.log('2---')
+        setMyFoods([...myFoods,e]);
+    }*/
+    function handleInputChange(e:string){
+        const food = allFoods.find(r => r.name === e);
+        if(!food){
+            throw new Error('Repositório já escolhido');
+        }
+        setMyFoods([...myFoods,food]);
     }
+
 
    useEffect(()=>{
     async function chargeFoods(){
         const response = await Api.get("getFoods",authorization);
-        setFoods(response.data);
-    }
-    
+        setAllFoods(response.data);
+    } 
     chargeFoods();
     },[]);
-    console.log(foods)
+    console.log('3---')
+    console.log(allFoods)
+    console.log(myFoods)
+    console.log('4---')
     return(
         <div>
             <h1>Home</h1>
-            {/* foods.map((result)=>(<option>{result.data}</option>)) */}
-            <Select  onChange={handleChange} defaultValue="Escolha um Alimento">
-                {foods.map(food =>
-                <Select.Option key={food.name} value={food.name}>{food.name}</Select.Option>
+            <Select  onChange={e=>handleInputChange(e)}>
+            
+                {allFoods.map(food =>
+                <Select.Option key={food.name} 
+                               value={food.name} 
+                               >
+                    {food.name}
+                    {/*<button onClick={()=>handleInputChange(food)}>+</button>*/}
+                </Select.Option>
                 )};
             </Select>
+            <List>
+                {myFoods.map(food=>(
+                    <li key={food.name}>
+                        <span>
+                            <DeleteButton onClick={()=>{}}>
+                                <FaTrash size={14} />
+                            </DeleteButton>
+                            {food.name}
+                        </span>
+                    </li>
+                ))}
+            </List>
         </div>
     );
 }
