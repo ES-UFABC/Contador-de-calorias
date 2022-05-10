b8ed7b77a56c9c16180f40f17eab2193b8517a4a
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-05-09 23:48:32 -0300

    meals list

--- front-end/src/pages/Historic/index.tsx
+++ front-end/src/pages/Historic/index.tsx
@@ -1,10 +1,47 @@
+import { useState, useEffect } from "react";
+import { List, Button } from "antd";
+import { FaTrash } from "react-icons/fa";
+import { Answer } from "../Home";
+import { Api } from "../../services/api";
+
+export interface MyAnswer extends Answer{
+    date: string,
+}
 
 export default function Historic(){
-    
+    const [meals,setMeals] = useState<MyAnswer[]>([]);
+
+    const token = localStorage.getItem('token');
+
+    const authorization = {
+        headers: {
+            Authorization: `${token}`
+        }
+    }
+
+    useEffect(()=>{
+        async function chargeFoods(){
+            const response = await Api.get("getMeals",authorization);
+            setMeals(response.data);
+        } 
+        
+        chargeFoods();
+        },[]);
 
     return(
         <div>
-            <h1>Histórico</h1>
+            <List>
+                {meals.map(meal=>(
+                    <li key={meal.date}>
+                        <span>
+                            <Button onClick={()=>{}}>
+                                <FaTrash size={14} />
+                            </Button>
+                            Refeição de: {meal.date}
+                        </span>
+                    </li>
+                ))}
+            </List>
         </div>
     );
 }
